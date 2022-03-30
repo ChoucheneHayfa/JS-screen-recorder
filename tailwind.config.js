1e5704c03e0ea17021a2b00cb232ded2b02cf376
ChoucheneHayfa <hayfa.chouchene1@mail.com>
2022-03-30 20:45:46 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  purge:[
+    "*.html"
+  ],
+  mode:"jit",
+  theme: {
+    extend: {},
+    fontFamily:{
+      sans:["Fira sans","sans-serif"]
+    }
+  },
+  plugins: [],
+}
